eee1d91b9399589843ed5d18c5566bfdfa5b1677
LancerQ <meekgot@gmail.com>
2025-03-07 17:39:51 +0300

    task item update

--- src/features/taskItem/index.ts
+++ src/features/taskItem/index.ts
@@ -8,7 +8,10 @@ export function taskItem(options: ITaskItem) {
   const { taskItem } = elementsNames
 
   const template = `
-    <div class="${taskItem}">${options.title}</div>
+    <div class="${taskItem}">
+      <div class="${taskItem}__check"></div>
+      <p class="${taskItem}__title">${options.title}</p>
+    </div>
   `
 
   return template
